a7f708c24fe03693840bb0b74acdcb8ba8e4333a
smb564 <supun.14@cse.mrt.ac.lk>
2017-01-18 01:07:25 +0530

    Added redirecting based on the user's group

--- SPSecond/Scripts/DefaultScript.js
+++ SPSecond/Scripts/DefaultScript.js
@@ -1,8 +1,37 @@
 ﻿$(document).ready(function () {
     // According to the user group page will be redirected
-    // TODO
+    var clientContext = SP.ClientContext.get_current();
+    var user = clientContext.get_web().get_currentUser();
+    var groups = user.get_groups();
 
-    // For now, redirect to the StudentView page
-    window.location.replace("StudentView.aspx?" + document.URL.split("?")[1]);
+    clientContext.load(groups);
 
+    clientContext.executeQueryAsync(function () {
+        var enumerator = groups.getEnumerator();
+
+        while (enumerator.moveNext()) {
+            var group = enumerator.get_current().get_title();
+
+            if (group === "Students") {
+                // A Student
+                window.location.replace("StudentView.aspx?" + document.URL.split("?")[1]);
+                break;
+            }
+
+            else if (group === "Admin") {
+                // Admin person
+                window.location.replace("AdminView.aspx?" + document.URL.split("?")[1]);
+                break;
+            }
+
+            else if (group === "Aluminai") {
+                // An Aluminai person
+                window.location.replace("AluminaiView.aspx?" + document.URL.split("?")[1]);
+                break;
+            }
+        }
+
+    }, function () {
+        alert("There's been some issue with getting data from the server. Try refreshing the page.");
+    });
 });
